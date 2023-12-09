15c5a0d1cc55149b6d2862b5b81d900111c06b7b
Anand <akanand104@gmail.com>
2023-12-10 02:12:02 +0530

    remove function done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,40 +11,54 @@
 */
 
 class Todo {
-  constructor(allTodos) {
-    this.todos = [];
+  constructor() {
+    this.task = [];
   }
-  add(todo) {
-    this.todos.push(todo);
+
+  add(newTask) {
+    this.task.push(newTask);
   }
-  remove(indexOfTodo) {
-    if (indexOfTodo >= this.todos.length) {
-      return null;
-    }
-    this.todos.splice(indexOfTodo, 1)
-  }
-  getAll() {
-    return this.todos;
-  }
-  get(indexOfTodo) {
-    if (indexOfTodo >= this.todos.length) {
-      return null;
-    }
-    return this.todos[indexOfTodo];
-  }
-  clear() {
-    this.todos.length = 0;
-  }
-  update(indexOfTodo, updatedTodo) {
-    if (indexOfTodo >= this.todos.length) {
-      return null;
-    }
-    this.todos[indexOfTodo] = updatedTodo;
-  }
-}
 
 
+  //  - remove(indexOfTodo): remove todo from list of todos - 
+  /* Method 1- splice 
+  method 2- loop */
+  remove(indexToRemove){
+    if( indexToRemove >= this.task.length){
+      //console.log("invalid input") 
+      return "invalid input"
+    } else {
+      this.task.splice(indexToRemove,1)
+    }
+  }
+
+  // second method - loop
+
+  /*  */
 
   
+}
 
-module.exports = Todo;
+//console.log(Todo);
+ let taskConstructor = new Todo();
+//  taskConstructor.add("tasknmber 2", "tasknumber 3"); // to pass multiple arugment you need to use spread operator
+
+taskConstructor.add("task 1")
+taskConstructor.add("task 2")
+taskConstructor.add("task 3")
+taskConstructor.add("task 4")
+console.log("before remove fn " , taskConstructor);
+taskConstructor.remove(5,1)
+
+console.log(taskConstructor);
+
+
+
+
+//  - remove(indexOfTodo): remove todo from list of todos
+/* For this question there are two methods one splice and other is using loop */
+let array = [1, 2, 3, 4, 5];
+array.splice(1, 2);
+// console.log(array);
+
+//module.exports = Todo;
