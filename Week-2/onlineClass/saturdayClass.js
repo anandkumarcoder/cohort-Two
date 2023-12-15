bb6566c591747b44da1a80ca5992d5cec98b5953
Anand <akanand104@gmail.com>
2023-12-15 23:43:30 +0530

    approach and comparison of promise

--- Week-2/onlineClass/saturdayClass.js
+++ Week-2/onlineClass/saturdayClass.js
@@ -2,18 +2,18 @@
 
 // callback function
 
-function square(n) {
+/* function square(n) {
   return n * n;
-}
+} */
 
-function sumofSomething(a, b, callbfn) {
+/* function sumofSomething(a, b, callbfn) {
   let value1 = callbfn(a);
   let value2 = callbfn(b);
 
   return value1 + value2;
 }
 
-let ans = sumofSomething(1, 2, square);
+let ans = sumofSomething(1, 2, square); */
 
 //console.log(ans);
 /* 
@@ -40,18 +40,18 @@ console.log("welcome to loop") */
 
 // reading a file
 
-const fs = require("fs");
+/* const fs = require("fs");
 
 fs.readFile("a.txt", "utf-8", function (err, data) {
   console.log(err);
   console.log(data);
 });
 
-console.log("done");
+console.log("done"); */
 
 //fs.write() is for writing a file
 
-function putCopyRightToFile(cb){
+/* function putCopyRightToFile(cb){
     fs.readFile("a.txt", "utf-8", function (err, data) {
       data = data + " copy right 2020 Anand kumar"
 
@@ -59,17 +59,47 @@ function putCopyRightToFile(cb){
         cb()
       })
       });
-}
+} */
 
 
-putCopyRightToFile(function () {
+/* putCopyRightToFile(function () {
     console.log("copyright has been put");
-})
+}) */
 
 
-setTimeout(() => {
+/* setTimeout(() => {
     console.log("First hi");
     setTimeout(()=>{
         console.log("inside the secondone");
     },2000)
-}, 1000);
+}, 1000); */
+
+// Promises
+
+// Aprroach 1
+
+function myOwnSetTimeout(fn, duration){
+  setTimeout(fn, duration)
+  // here set time is taking its argument from the function's parameter
+}
+
+myOwnSetTimeout(function () {
+  console.log("hi there");
+},3000)
+
+// approach 2 - with promise 
+
+function myOwnSetTimeout2(duration) {
+  let p = new Promise((resolve) => {
+    setTimeout(resolve,1000)
+  })
+
+  return p
+}
+
+myOwnSetTimeout2(1000)
+.then(function () {
+  console.log("log the first thing");
+})
+
+// then is for consumption of the promise
