b527c5f00ae66e2a5ac75137bf920e8b5949762c
Anand <akanand104@gmail.com>
2023-12-24 00:57:17 +0530

    how to read body data

--- Week-2/onlineClass/sundayClass.js
+++ Week-2/onlineClass/sundayClass.js
@@ -1,10 +1,35 @@
 const express = require('express')
+
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+//   res.send('Hello World!')
+  res.send('Hello World2')
+
+  /* res.json({
+    name : "Anand"
+  }) */
+
 })
+
+
+app.use(bodyParser.json())
+
+app.post('/tryouts', (req, res) => {
+    //   res.send('Hello World!')
+      res.send('Hello World2 tryouts')
+      console.log(req.headers);
+      console.log(req.body);
+    
+      /* res.json({
+        name : "Anand"
+      }) */
+    
+    })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
