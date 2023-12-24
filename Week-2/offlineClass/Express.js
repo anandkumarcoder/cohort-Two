48d651dfd2c4c27b6680726f2165ad417802ccb5
Anand <akanand104@gmail.com>
2023-12-24 23:21:50 +0530

    logic for post,put  and delete added

--- Week-2/offlineClass/Express.js
+++ Week-2/offlineClass/Express.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 //simple logic
 /* function sum(n){
     let ans =0
@@ -30,27 +32,27 @@ const users = [
       {
         healthy: false,
       },
-      {
-        healthy: false,
-      },
+    //   {
+    //     healthy: false,
+    //   },
     ],
   },
 ];
 
-// john needs to know how many kidneys he has an dhow many are healthy and unhealthy
+// john needs to know how many kidneys he has an dhow many are healthy and unhealthy- use of query parameter
 app.get("/", function (req, res) {
   const johnKidneys = users[0].kidneys;
   const numberOfKidneys = johnKidneys.length;
-  console.log(numberOfKidneys);
+//   console.log(numberOfKidneys);
   let numberOfHealthyKidneys = 0;
 
   for (let i = 0; i < johnKidneys.length; i++) {
     if (johnKidneys[i].healthy) {
-        console.log(johnKidneys[i].healthy);
+        // console.log(johnKidneys[i].healthy);
       numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
     }
   }
-console.log(numberOfHealthyKidneys);
+// console.log(numberOfHealthyKidneys);
   const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys
 
   res.json({
@@ -59,5 +61,59 @@ console.log(numberOfHealthyKidneys);
     numberOfUnhealthyKidneys
   })
 });
+
+
+
+// use of post request
+
+app.post("/", function(req,res){
+   // console.log(req.body) // returns undefined
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy : isHealthy
+    })
+
+    res.json({
+        msg : "done"
+    })
+})
+
+
+
+// put handler - update all kidney of the user to be healthy
+
+app.put("/", function(req, res){
+    console.log("from here " + users[0].kidneys.length);
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        
+        users[0].kidneys[i].healthy = true
+    }
+
+    res.json({})
+})
+
+
+// delete method -- remove all the unhealthy kidneys
+
+app.delete("/", function(req, res){
+    const newKidneyArray = []
+
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+        if (users[0].kidneys[i].healthy) {
+            newKidneyArray.push({
+                healthy : true
+            })
+        }
+
+        console.log(newKidneyArray);
+        users[0].kidneys = newKidneyArray
+
+        res.json({msg: "done"})
+    }
+
+
+   
+})
+
 
 app.listen(3000);
