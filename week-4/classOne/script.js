53186cdca5982ad1259f275a8954d89c635d95d8
Anand <akanand104@gmail.com>
2023-12-25 20:16:16 +0530

    sum logic done with BEs

--- week-4/classOne/script.js
+++ week-4/classOne/script.js
@@ -3,11 +3,42 @@ function populateDiv() {
 
     const firstValue = document.querySelector("#firstNumber").value
     const secondValue = document.querySelector("#secondNumber").value
-    const element = document.querySelector("#final-sum");
+     const element = document.querySelector("#final-sum");
+// https://sum-server.100xdevs.com/sum?a=8&b=5
 
-    
+
+// hitting the server to do the calcluation
+/* fetch("https://sum-server.100xdevs.com/sum?a=" + firstValue + "&b=" + secondValue).then(function(response){
+    console.log(response);
+    response.text() //this is also promise
+    .then(function(ans){
+       // console.log(ans);
+        element.innerHTML = ans
+
+    })
+})
+    */
+   
+
+
     //console.log(element); // typeof element is an object
 
     // element.innerHTML = "hello World 2"
-    element.innerHTML = parseInt(firstValue) + parseInt(secondValue)
+    // element.innerHTML = parseInt(firstValue) + parseInt(secondValue)
+}
+
+// Better way to do the above
+
+async function populateDiv2(){
+    const firstValue = document.querySelector("#firstNumber").value
+    const secondValue = document.querySelector("#secondNumber").value
+     const element = document.querySelector("#final-sum");
+
+     const response = await fetch("https://sum-server.100xdevs.com/sum?a=" + firstValue + "&b=" + secondValue )
+
+     console.log(response);
+
+     const ans = await response.text();
+     element.innerHTML = ans
+
 }
