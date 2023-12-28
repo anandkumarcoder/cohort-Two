9f1fba914bc766407c7a3936ed1bb5c027c15470
Anand <akanand104@gmail.com>
2023-12-28 13:07:35 +0530

    built: precheck logic done

---
+++ Week-3/3.1/Script.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.get("/health-checkup", (req, res) => {
+  const kidneyID = req.query.kidneyID;
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  if (!(username === "anand" && password === "pass")) {
+    res.status(400).json({
+      msg: "something is not right with authentication",
+    });
+  }
+
+  if (kidneyID != 1 || kidneyID != 2) {
+    res.status(400).json({
+      msg: "something is not right with kidney",
+    });
+  }
+
+  res.json({
+    msg: "Your kidney is fine",
+  });
+});
+
+app.listen(3000);
